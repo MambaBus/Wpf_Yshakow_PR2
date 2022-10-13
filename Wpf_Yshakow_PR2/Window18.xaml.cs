0a4e18e0eee94073e6502b6b4c86c5d1ce4b4908
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-10-13 16:19:41 +1000

    Не доделанный слайдер

--- Wpf_Yshakow_PR2/Window18.xaml.cs
+++ Wpf_Yshakow_PR2/Window18.xaml.cs
@@ -23,5 +23,44 @@ namespace Wpf_Yshakow_PR2
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.planhet.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            planhet.DefaultDrawingAttributes.Width = ((Slider)sender).Value;
+            planhet.DefaultDrawingAttributes.Height = ((Slider)sender).Value;
+        }
+
+        private void Red_Click(object sender, RoutedEventArgs e)
+        {
+            planhet.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void Green_Click(object sender, RoutedEventArgs e)
+        {
+            planhet.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void Blue_Click(object sender, RoutedEventArgs e)
+        {
+            planhet.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void Black_Click(object sender, RoutedEventArgs e)
+        {
+            planhet.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        private void lastik_Checked(object sender, RoutedEventArgs e)
+        {
+            if (planhet.EditingMode == InkCanvasEditingMode.Ink)
+                planhet.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            else
+                planhet.EditingMode = InkCanvasEditingMode.Ink;
+        }
     }
 }
